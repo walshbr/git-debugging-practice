1b3cf367d853c394c2847183179693423deda2c8
walshbr <bmw9t@virginia.edu>
2018-10-15 10:40:33 -0400

    Initial commits

---
+++ pirates.py
@@ -0,0 +1,10 @@
+# prompt the user to find out if they are a pirate. If they are, respond accordingly.
+
+# what is the code supposed to do? work line by line to troubleshoot.
+# each time you think you've made a correction and gotten it closer, push those changes up.
+
+greeting = input("Hello, possible pirate! What's the password?)
+if greeting in ["Arrr!"):
+	print("Go away, pirate.")
+elif
+print("Greetings, hater of pirates!")
